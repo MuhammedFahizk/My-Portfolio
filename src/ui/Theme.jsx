ae556af6ccadecbbdefff5d0e09c1a8115f1d612
MuhammedFahizk <fahizk100@gmail.com>
2025-07-05 15:15:41 +0530

    theme are store to local storage

--- src/ui/Theme.jsx
+++ src/ui/Theme.jsx
@@ -5,10 +5,24 @@ export const Theme = () => {
   const [darkMode, setDarkMode] = useState(false)
 
  useEffect(() => {
-    if(darkMode) {
-        document.documentElement.classList.remove('dark');
-    }else {
-        document.documentElement.classList.add('dark');
+    if (localStorage.getItem('color-theme')) {
+        if (localStorage.getItem('color-theme') === 'light') {
+            document.documentElement.classList.add('dark');
+            localStorage.setItem('color-theme', 'dark');
+        } else {
+            document.documentElement.classList.remove('dark');
+            localStorage.setItem('color-theme', 'light');
+        }
+
+    // if NOT set via local storage previously
+    } else {
+        if (document.documentElement.classList.contains('dark')) {
+            document.documentElement.classList.remove('dark');
+            localStorage.setItem('color-theme', 'light');
+        } else {
+            document.documentElement.classList.add('dark');
+            localStorage.setItem('color-theme', 'dark');
+        }
     }
  }, [darkMode])
  
